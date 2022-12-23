f91bf113600b7a7af0d17c1aee0860a484c5f0a5
Vadim <vibladosik@mail.ru>
2022-12-23 13:07:56 +0300

    Новое дз

--- Program.cs
+++ Program.cs
@@ -1,77 +1,79 @@
-﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
 
-// Console.WriteLine("Введите первое число: ");
-// int num_A = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число: ");
-// int num_B = Convert.ToInt32(Console.ReadLine());
-// if (num_A > num_B)
+// Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+// while(true)
 // {
-//      Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
-//  }
-//  else
-// {
-//      Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+//     string input = Console.ReadLine();
+//     if(!input.Equals("exit"))
+//         Console.WriteLine("{0}->{1}",input, input[1]);
 // }
 
 
-// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-// Console.WriteLine("Введите 3 числа:");
-// int num_1 = Convert.ToInt32(Console.ReadLine());
-// int num_2 = Convert.ToInt32(Console.ReadLine());
-// int num_3 = Convert.ToInt32(Console.ReadLine());
-// int max = num_1;
-// if (num_2 > max)
+// int number = ReadInt("Введите число: ");
+// int count = number.ToString().Length;
+// Console.Write(MakeArray(number, count));
+// int ReadInt(string message)
 // {
-//      max = num_2;
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
 // }
-// if (num_3 > max)
+// int MakeArray(int a, int b)
 // {
-//      max = num_3;
-//  }
-//  Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+//     int result = 0;
+//     if (b < 3)
+//     {
+//         Console.Write("Третьей цифры нет, держи: ");
+//     }
+//     else
+//     {
+//         int c = 1;
+//         for (int i = b; i > 3; i--)
+//         {
+//             c = c * 10;
+//         }
 
-// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
-
-// Console.WriteLine("Введите число:");
-// int N = Convert.ToInt32(Console.ReadLine());
-// if (N % 2 == 1)
-// {
-//     Console.WriteLine("Число " + N + " является: НЕЧЁТНЫМ");
-// }
-// else
-// {
-//     Console.WriteLine("Число " + N + "является: ЧЁТНЫМ");
+//         result = (a / c) % 10;
+//     }
+//     return result;
 // }
 
-// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
 
-//  int i = 1;
-//  bool not = true;
-//  Console.WriteLine("Введите число:");
-//  int N = Convert.ToInt32(Console.ReadLine());
-//  Console.WriteLine("Чётные числа от 1 до " + N);
-//  while (i <= N)
-//  {
-//      if (i % 2 != 1)
-//      {
-//          Console.Write(i + ", ");
-//          not = false;
-//      }
-//      i++; }
-//  if (not)
+// int dayNumber = ReadInt("Введите число от 1 до 7: ");
+// Console.WriteLine(WorkHoliday(dayNumber));
+// int ReadInt(string message)
 // {
-//      Console.WriteLine("Нет чётных чисел!");
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// string WorkHoliday(int a)
+// {
+//     if (a > 0 && a < 8)
+//     {
+//         if (a == 7 || a == 6)
+//         {
+//             Console.Write("Под цифрой " + a + " - Выходной");
+//         }
+//         else
+//         {
+//             Console.Write("Под цифрой " + a + " - Рабочий");
+//         }
+//     }
+//     else
+//     {
+//         Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+//     }
+//     return " день.";
 // }
