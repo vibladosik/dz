35c31b2cc1e33ad5d58572a4b899eebfef42a3c5
Vadim <vibladosik@mail.ru>
2022-12-23 15:00:02 +0300

    3 домашняя работа

--- Program.cs
+++ Program.cs
@@ -1,79 +1,64 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
 
-// Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
-// while(true)
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int len = number.Length;
+// if (len == 5)
 // {
-//     string input = Console.ReadLine();
-//     if(!input.Equals("exit"))
-//         Console.WriteLine("{0}->{1}",input, input[1]);
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - НЕ палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
 // }
 
 
-// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// int number = ReadInt("Введите число: ");
-// int count = number.ToString().Length;
-// Console.Write(MakeArray(number, count));
+// int x1 = ReadInt("Введите координату X первой точки: ");
+// int y1 = ReadInt("Введите координату Y первой точки: ");
+// int z1 = ReadInt("Введите координату Z первой точки: ");
+// int x2 = ReadInt("Введите координату X второй точки: ");
+// int y2 = ReadInt("Введите координату Y второй точки: ");
+// int z2 = ReadInt("Введите координату Z второй точки: ");
+
+// int A = x2 - x1;
+// int B = y2 - y1;
+// int C = z1 - z2;
+
+// double length = Math.Sqrt(A * A + B * B + C * C);
+// Console.WriteLine($"Длинна отрезка {length}");
 // int ReadInt(string message)
 // {
 //     Console.Write(message);
 //     return Convert.ToInt32(Console.ReadLine());
 // }
-// int MakeArray(int a, int b)
-// {
-//     int result = 0;
-//     if (b < 3)
-//     {
-//         Console.Write("Третьей цифры нет, держи: ");
-//     }
-//     else
-//     {
-//         int c = 1;
-//         for (int i = b; i > 3; i--)
-//         {
-//             c = c * 10;
-//         }
 
-//         result = (a / c) % 10;
-//     }
-//     return result;
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// int number = ReadInt("Введите число N: ");
+// for (int i = 1; i <= number; i++)
+// { 
+//     Console.Write($"{i*i*i} ");
 // }
-
-
-// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-// 6 -> да
-// 7 -> да
-// 1 -> нет
-
-// int dayNumber = ReadInt("Введите число от 1 до 7: ");
-// Console.WriteLine(WorkHoliday(dayNumber));
+// // Функция ввода сообщения
 // int ReadInt(string message)
 // {
 //     Console.Write(message);
 //     return Convert.ToInt32(Console.ReadLine());
-// }
-// string WorkHoliday(int a)
-// {
-//     if (a > 0 && a < 8)
-//     {
-//         if (a == 7 || a == 6)
-//         {
-//             Console.Write("Под цифрой " + a + " - Выходной");
-//         }
-//         else
-//         {
-//             Console.Write("Под цифрой " + a + " - Рабочий");
-//         }
-//     }
-//     else
-//     {
-//         Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
-//     }
-//     return " день.";
 // }
