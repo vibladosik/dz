b33ea970e7ab5e76a8446f8f49d153172bc9d0d2
Vadim <vibladosik@mail.ru>
2023-01-05 14:09:13 +0300

    Новое дз урок 9

--- Program.cs
+++ Program.cs
@@ -493,3 +493,83 @@
 //         Console.WriteLine();
 //     }
 // }
+
+
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.WriteLine("Введите натуральное число больше 1:");
+// int number = int.Parse(Console.ReadLine());
+
+// ///Метод вывода натуральных чисел от N до 1:
+// void NumberCounter (int number)
+// {
+//     if (number < 0) Console.Write($"{number} не натуральное число");
+//     if (number == 0) return;
+//     Console.Write("{0,4}", number);
+//     NumberCounter (number - 1);
+// }
+// NumberCounter(number);
+
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("Введите начальное число M:");
+// int numberM = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите начальное число M:");
+// int numberN = int.Parse(Console.ReadLine());
+
+// ///Метод нахождения суммы натуральных элементов в промежутке от M до N
+// void NumberSum (int numberM, int numberN, int sum)
+// {
+//     if (numberM > numberN) 
+//     {
+//         Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+//         return;
+//     }
+//     sum = sum + (numberM++);
+//     NumberSum(numberM, numberN, sum);
+// }
+// NumberSum(numberM, numberN, 0);
+
+
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// AkkermanFunction(m,n);
+
+
+// // вызов функции Аккермана
+// void AkkermanFunction(int m, int n)
+// {
+//     Console.Write(Akkerman(m, n)); 
+// }
+
+// // функция Аккермана
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
